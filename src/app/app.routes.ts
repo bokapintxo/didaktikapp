50e1a3ed115669e357c89e47d0f696b985d672a9
Erlantz <96289199+erlgr@users.noreply.github.com>
2024-01-23 13:56:30 +0100

    plantillak

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,5 +9,13 @@ export const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
+  },  {
+    path: 'template-page',
+    loadComponent: () => import('./template-page/template-page.page').then( m => m.TemplatePagePage)
   },
+  {
+    path: 'dialog',
+    loadComponent: () => import('./dialog/dialog.page').then( m => m.DialogPage)
+  },
+
 ];
