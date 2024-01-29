5dec9e3c7fba517422b5b01d26c0c000ea5e3546
AimarMurua <murua.aimar@uni.eus>
2024-01-29 09:47:44 +0100

    Hitzak lotu funtzionatzen, aldaketa gutxi falta dira
    Kodigoaren zati gehiena funtzionatzen dago, gauza txiki batzu gehiago sartzea falta da.

--- src/app/hitzak-lotu/hitzak-lotu.page.ts
+++ src/app/hitzak-lotu/hitzak-lotu.page.ts
@@ -12,22 +12,50 @@ import { IonicModule } from '@ionic/angular';
 })
 export class HitzakLotuPage implements OnInit {
 
-  estilosBotones: { [key: string]: string } = {}; // // Botoi bakotzarentza klase desberdinak gordetzen ditu
-  botonConEstilo: string = '';
+  estilosBotones: { [key: string]: string } = {};
+  parejasFormadas: { [key: string]: boolean } = {};
+  botonesSeleccionados: { [key: string]: boolean } = {};
 
   estiloaAldatu(nombreBoton: string, claseEstilo: string) {
-
-    if (this.botonConEstilo && this.botonConEstilo !== nombreBoton) {
-      return; // Botoi batek estiloa jarrita badauka, ez dio utziko beste botoiei estiloa aldatzen
+    if (this.tieneParejaYColor(nombreBoton) || this.botonesSeleccionados[nombreBoton]) {
+      return;
     }
-
-    // Clasea jarrita badu, clickarekin kendu, kontrakoz, gehitu egiten du
-    this.estilosBotones[nombreBoton] = this.estilosBotones[nombreBoton] === claseEstilo ? '' : claseEstilo;
-
-    this.botonConEstilo = this.estilosBotones[nombreBoton] === '' ? '' : nombreBoton;
+  
+    if (Object.keys(this.botonesSeleccionados).length < 2) {
+      const esPrimerConjunto = nombreBoton === 'eguenzuri' || nombreBoton === 'bariku_argi' || nombreBoton === 'zapatu_erregularra' || nombreBoton === 'domeka_karnabala' || nombreBoton === 'ilen_karnabala' || nombreBoton === 'martitzen_karnabala' || nombreBoton === 'txitxiburduntxi';
+  
+      // Si es el primer conjunto o ya se ha seleccionado un botón del primer conjunto
+      if (esPrimerConjunto || Object.keys(this.botonesSeleccionados).length > 0) {
+        this.botonesSeleccionados[nombreBoton] = true;
+        this.estilosBotones[nombreBoton] = claseEstilo;
+  
+        if (Object.keys(this.botonesSeleccionados).length === 2) {
+          const botonesSeleccionadosArray = Object.keys(this.botonesSeleccionados);
+          const primerBoton = botonesSeleccionadosArray[0];
+          const segundoBoton = botonesSeleccionadosArray[1];
+  
+          if (this.sonPareja(primerBoton, segundoBoton)) {
+            this.parejasFormadas[primerBoton] = true;
+            this.parejasFormadas[segundoBoton] = true;
+            this.botonesSeleccionados = {};
+          } else {
+            // Reiniciar la información de los botones inmediatamente
+            this.estilosBotones[primerBoton] = this.parejasFormadas[primerBoton] ? claseEstilo : '';
+            this.estilosBotones[segundoBoton] = this.parejasFormadas[segundoBoton] ? claseEstilo : '';
+            this.botonesSeleccionados = {};
+          }
+        }
+      }
+    }
   }
 
-  
+  sonPareja(boton1: string, boton2: string): boolean {
+    return this.estilosBotones[boton1] === this.estilosBotones[boton2] && this.estilosBotones[boton1] !== '';
+  }
+
+  tieneParejaYColor(boton: string): boolean {
+    return this.parejasFormadas[boton];
+  }
 
   constructor() { }
 
