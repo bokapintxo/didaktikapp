26ec36dc722396e9f9f94a2815ff2681bdaffb8d
Erlantz <git@erl.gr>
2024-02-11 18:42:48 +0100

    aldaketa oso txikiak npm erabili baino lenuo (bildurra emateit)

--- src/app/amaiera/amaiera.page.ts
+++ src/app/amaiera/amaiera.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { Haptics } from '@capacitor/haptics';
 
 @Component({
   selector: 'app-amaiera',
@@ -11,10 +13,28 @@ import { IonicModule } from '@ionic/angular';
   imports: [IonicModule, CommonModule, FormsModule]
 })
 export class AmaieraPage implements OnInit {
+  audioBtn: any;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    this.audioBtn = new Audio();
+    this.audioBtn.src = '../../assets/aud/btn_handia.mp3';
   }
+
+  navigateKredituak() {
+    this.router.navigate(['/kredituak']);
+  }
+
+  async pushButton() {
+    this.audioBtn.load();
+    this.audioBtn.play();
+    await Haptics.vibrate({duration: 10});
+    await new Promise(resolve => setTimeout(resolve, 50));
+    await Haptics.vibrate({duration: 10});
+  }
+
+
+
 
 }
